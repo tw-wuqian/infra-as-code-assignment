cf243dcef9a5321dc5272a1c2ca51af15ee9caef
qian-otr <thoughtworks.tw.wu@extaccount.com>
2024-12-30 16:25:06 +0800

    add backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     key            = "wei-1/dev/terraform.tfstate"
-#     region         = "eu-central-1"
-#     dynamodb_table = "wei-1-tfstate-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    key            = "wei-1/dev/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "wei-1-tfstate-locks"
+    encrypt        = true
+  }
+}
