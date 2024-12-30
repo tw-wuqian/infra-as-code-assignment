402d20c6548e1be1f375c15492ee290ea00ec372
qian-otr <thoughtworks.tw.wu@extaccount.com>
2024-12-30 18:42:56 +0800

    update s3 name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    key            = "wei-bucket-1/dev/terraform.tfstate"
+    bucket = "wei-bucket-1"
+    key            = "wei-1/dev/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "wei-1-tfstate-locks"
     encrypt        = true
