fabd7af8d990a206533afa47949465986e70a815
qian-otr <thoughtworks.tw.wu@extaccount.com>
2025-01-02 19:20:30 +0800

    add policy

--- terraform/modules/iam/iam.tf
+++ terraform/modules/iam/iam.tf
@@ -29,6 +29,35 @@ resource "aws_iam_policy" "lambda_policy" {
   })
 }
 
+resource "aws_iam_role_policy" "lambda_policy" {
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ],
+        Resource = aws_dynamodb_table
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "attach_policy" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
