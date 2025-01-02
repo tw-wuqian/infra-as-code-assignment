067ac49d62d06417d94343f43d78bf4acddc597e
qian-otr <thoughtworks.tw.wu@extaccount.com>
2025-01-02 19:45:08 +0800

    add more log

--- terraform/modules/lambda_api_gateway/register_user.py
+++ terraform/modules/lambda_api_gateway/register_user.py
@@ -4,11 +4,16 @@ from urllib.parse import parse_qsl
 
 
 def lambda_handler(event, context):
+    print("Received event:", event)
     query_string = dict(parse_qsl(event["rawQueryString"]))
+    print("Parsed query string:", query_string)
     client = boto3.resource("dynamodb")
+
     db_table = client.Table(getenv("DB_TABLE_NAME"))
     try:
+        print("Attempting to insert item:", query_string)
         response = db_table.put_item(Item=query_string)
+        print("DynamoDB put_item response:", response)
         return { "message": "Registered User Successfully" }
     except Exception as error_details:
         print(error_details)
