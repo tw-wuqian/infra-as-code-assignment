cffc1cab733df434bd31bda7651ee3a35cfe04cf
qian-otr <thoughtworks.tw.wu@extaccount.com>
2025-01-03 16:48:08 +0800

    rename function

--- terraform/modules/lambda_api_gateway/lambda_api_gateway.tf
+++ terraform/modules/lambda_api_gateway/lambda_api_gateway.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "register_user" {
   role             = var.lambda_role_arn
   handler          = "register_user.lambda_handler"
   runtime          = "python3.8"
-  source_code_hash = filebase64sha256("${path.module}/register_verify_user.zip")
+  source_code_hash = filebase64sha256("${path.module}/register_user.zip")
   environment {
     variables = {
       DB_TABLE_NAME = "wei-1-tfstate-locks"
